ab61d4c95289b9d6d2e98b57a2914f5ee565936a
= <wjdrlans4000@naver.com>
2021-05-24 22:23:21 +0900

    rest api 프로젝트 생성

---
+++ rest-api-with-springboot/src/main/java/me/rest/restapiwithspringboot/RestApiWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package me.rest.restapiwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWithSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiWithSpringbootApplication.class, args);
+    }
+
+}
---
+++ rest-api-with-springboot/src/test/java/me/rest/restapiwithspringboot/RestApiWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rest.restapiwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWithSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
