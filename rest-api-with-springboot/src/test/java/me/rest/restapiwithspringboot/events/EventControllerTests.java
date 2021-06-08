622573c63f9709cce7e3c00a3cb399264d240564
= <wjdrlans4000@naver.com>
2021-06-08 22:19:44 +0900

    주석보완

--- rest-api-with-springboot/src/test/java/me/rest/restapiwithspringboot/events/EventControllerTests.java
+++ rest-api-with-springboot/src/test/java/me/rest/restapiwithspringboot/events/EventControllerTests.java
@@ -44,7 +44,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc // 모킹을 사용하지않고 실제 리파지토리를 사용하여 테스트 동작
 @AutoConfigureRestDocs
 @Import(RestDocsConfiguration.class) //다른 스프링 bean 설정파일을 읽어와서 사용하는 방법 중 하나
-@ActiveProfiles("test")
+@ActiveProfiles("test") //test application properties를 추가로 사용
 public class EventControllerTests {
 
     /*
