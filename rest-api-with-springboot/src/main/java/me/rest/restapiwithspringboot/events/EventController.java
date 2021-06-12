a39e94ab7391b0fb2a1eb635f2247f695928444a
= <wjdrlans4000@naver.com>
2021-06-12 22:31:45 +0900

    이벤트 조회 api 구현

--- rest-api-with-springboot/src/main/java/me/rest/restapiwithspringboot/events/EventController.java
+++ rest-api-with-springboot/src/main/java/me/rest/restapiwithspringboot/events/EventController.java
@@ -12,13 +12,11 @@ import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.Optional;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -91,6 +89,19 @@ public class EventController {
         pagedResources.add(new Link("/docs/index.html#resources-events-list").withRel("profile"));
         return ResponseEntity.ok(pagedResources);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity  getEvent(@PathVariable Integer id){
+        Optional<Event> optionalEvent = this.eventRepository.findById(id);
+        if(optionalEvent.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        Event event = optionalEvent.get();
+        EventResource eventResource = new EventResource(event);
+        eventResource.add(new Link("/docs/index.html#resources-events-get").withRel("profile"));
+        return ResponseEntity.ok(eventResource);
+    }
+
 
     private ResponseEntity badRequest(Errors errors){
         return ResponseEntity.badRequest().body(new ErrorsResource(errors));
