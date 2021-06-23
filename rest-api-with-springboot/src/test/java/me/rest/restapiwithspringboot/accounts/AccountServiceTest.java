cbd5606a7560fd2d1e196c8f197e8a7646e8f346
= <wjdrlans4000@naver.com>
2021-06-23 21:14:19 +0900

    다양한 예외를 테스트하자
    예외 테스트하는 3가지 방법

--- rest-api-with-springboot/src/test/java/me/rest/restapiwithspringboot/accounts/AccountServiceTest.java
+++ rest-api-with-springboot/src/test/java/me/rest/restapiwithspringboot/accounts/AccountServiceTest.java
@@ -1,23 +1,31 @@
 package me.rest.restapiwithspringboot.accounts;
 
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class AccountServiceTest {
+
+    @Rule
+    public ExpectedException expectedException;
 
     @Autowired
     AccountService accountService;
@@ -49,6 +57,32 @@ public class AccountServiceTest {
         assertThat(userDetails.getPassword()).isEqualTo(password)
 
         ;
+    }
+
+//    다양한 예외 테스트하는 방법.
+//    1. @Test(expected = UsernameNotFoundException.class)
+    @Test
+    public void findByUsernameFail(){
+        String username = "random@email.com";
+
+        //2.
+        //훨씬 많은걸 테스트할수 있음
+//        try {
+//            accountService.loadUserByUsername(username);
+//            fail("supposed to be failed");
+//        }catch (UsernameNotFoundException e){
+//            assertThat(e.getMessage()).containsSequence(username);
+//        }
+
+        //3. 발생할(예측되는) 예외를 미리 적어줘야함
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(username));
+
+        // When
+        accountService.loadUserByUsername(username);
+
+
+
     }
 
 }
